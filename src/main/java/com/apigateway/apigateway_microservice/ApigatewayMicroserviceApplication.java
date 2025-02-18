22fd5f53277690be92b945a845673f1f2e78514a
LudwinNtt <lsuarein@emeal.nttdata.com>
2025-02-18 18:05:27 -0500

    subiendo api gateway

---
+++ src/main/java/com/apigateway/apigateway_microservice/ApigatewayMicroserviceApplication.java
@@ -0,0 +1,15 @@
+package com.apigateway.apigateway_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ApigatewayMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApigatewayMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apigateway/apigateway_microservice/ApigatewayMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apigateway.apigateway_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
